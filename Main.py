4595ae799a664ee16c60508019e0fe7ee30445c0
kimsoyeong <iloveit356@gmail.com>
2020-08-09 03:51:36 +0900

    Main 코드 추가 및 수정
    - 로그인(login) 함수
    - 회원가입(signup) 함수
    -  메시지박스(알림창) 함수
    - server Data 읽기 함수
    - data file 서버 업로드 함수

--- Main.py
+++ Main.py
@@ -1,26 +1,29 @@
+import ctypes
 import json
+import os
 import queue
 import sys
 import threading
 import urllib.request
 from collections import OrderedDict
+from ftplib import FTP
+from io import BytesIO
 
 import cv2
 from PyQt5 import QtCore, QtGui, uic, QtWidgets
 from PyQt5.QtWidgets import QApplication, QAction, qApp
 
-
 # load both ui file
-uifile_1 = 'Start.ui'
+uifile_1 = 'Start.ui'  # start
 form_1, base_1 = uic.loadUiType(uifile_1)
 
-uifile_2 = 'loginui.ui'
+uifile_2 = 'loginui.ui'  # login
 form_2, base_2 = uic.loadUiType(uifile_2)
 
-uifile_3 = 'Sign.ui'
+uifile_3 = 'Sign.ui'  # sign up
 form_3, base_3 = uic.loadUiType(uifile_3)
 
-uifile_4 = 'simple.ui'
+uifile_4 = 'simple.ui'  # main
 form_4, base_4 = uic.loadUiType(uifile_4)
 
 
@@ -62,8 +65,8 @@ class loginPage(base_2, form_2):
     def __init__(self):
         super(base_2, self).__init__()
         self.setupUi(self)
-        self.Back.clicked.connect(self.change)
-        self.OK.clicked.connect(self.change_2)
+        self.btnBack.clicked.connect(self.change)
+        self.btnLogin.clicked.connect(self.login)
         self.Exit_button()
 
     def Exit_button(self):
@@ -81,22 +84,44 @@ class loginPage(base_2, form_2):
         self.setWindowTitle('Menubar')
         self.show()
 
-    def change(self):
+    def change(self):  # 시작화면으로 돌아가기
         self.window = Start()
         self.window.show()
         self.close()
 
-    def change_2(self):
+    def change_2(self):  # 메인 페이지로 화면 전환
         self.window = MainPage()
         self.window.show()
         self.close()
+
+    def login(self):  # 로그인
+        loginOk = 0  # 로그인 성공 여부
+        userid = self.inputId.text()
+        userpw = self.inputPw.text()
+        if userid != '' and userpw != '':
+            serverData = readServerData("jsonTest")  # 서버 데이터 읽어옴
+            for i in serverData:
+                if serverData[i]['id'] == userid: # check id
+                    if serverData[i]['pw'] == userpw: # check pw
+                        # username = serverData[i]['name']
+                        loginOk = 1
+                        break
+
+        if loginOk:  # 로그인 성공
+            # TODO : Login 정보 업로드 - 페이지 전환에 따라 저장된 전역변수값 전달X
+            #        1. Server에 로그인 정보 저장 - 다른 유저의 로그인 정보와 혼선 가능
+            #        2. 프로그램이 위치한 폴더에 text파일 생성 - 로그아웃 시 삭제 필요
+            self.change_2()  # 메인 페이지로 이동
+        else:  # 로그인 실패
+            messageBox("로그인 실패", "ID 또는 PW가 틀렸습니다.", 0)
 
 
 class SignPage(base_3, form_3):
     def __init__(self):
         super(base_3, self).__init__()
         self.setupUi(self)
-        self.Back.clicked.connect(self.change)
+        self.btnBack.clicked.connect(self.change)
+        self.btnSignup.clicked.connect(self.signUp)
         self.Exit_button()
 
     def Exit_button(self):
@@ -119,7 +144,53 @@ class SignPage(base_3, form_3):
         self.window.show()
         self.close()
 
+    def signUp(self):  # signup 회원가입
+        userid = self.inputId.text()
+        userpw = self.inputPw.text()
+        username = self.inputName.text()
 
+        if userid == '' or userpw == '' or username == '':
+            messageBox("경고", "ID, PASSWORD와 이름은 필수 항목입니다.", 0)
+        else:
+            file_data = readServerData("jsonTest")
+            # TODO: id, pw, 이름 중복 검사 해주기
+
+            userNum = 'user' + str(len(file_data))
+            file_data.setdefault('t', {})  # key값에 변수로는 왜 바로 안되는지 모르겠네
+            file_data['t']['id'] = userid
+            file_data['t']['pw'] = userpw
+            file_data['t']['name'] = username
+            file_data['t']['age'] = self.spinBox.value()
+            file_data['t']['balance'] = 0  # default
+            file_data['t']['challenge'] = False  # default
+            file_data[userNum] = file_data.pop('t')  # user 번호 설정
+
+            upload(file_data, "jsonTest")  # 서버에 새로운 회원 정보 업로드
+            i = messageBox("가입완료", "회원가입이 완료되었습니다.", 0)
+            if i == 1:
+                self.change()
+
+
+# upload data file to server _ 서버에 업로드
+def upload(file_data, fileName):
+    os.system("wget http://soyeong99.dothome.co.kr/web/"+fileName+".json")
+    ftp = FTP('112.175.184.79', 'soyeong99', 'thdud4869!')
+    ftp.cwd("/html/web/")
+    tempF = json.dumps(file_data, indent="\t")
+    tempF = bytes(tempF, "utf8")
+    file_like = BytesIO(tempF)
+    ftp.storbinary('STOR jsonTest.json', file_like)
+
+
+# server Data를 읽어옴
+def readServerData(fileName):
+    file_data = OrderedDict()
+    data = urllib.request.urlopen("http://soyeong99.dothome.co.kr/web/"+fileName+".json").read()
+    file_data = json.loads(data)
+    return file_data
+
+
+# Main Page - Camera
 running = False
 capture_thread = None
 q = queue.Queue()
@@ -173,8 +244,10 @@ class MainPage(QtWidgets.QMainWindow, form_4):
         QtWidgets.QMainWindow.__init__(self, parent)
         self.setupUi(self)
 
-        self.btnCamera.clicked.connect(self.start_clicked)
-        self.btnLogout.clicked.connect(self.log_out)
+        # TODO : login한 user의 이름 표시
+        # self.loginName.setText(logName)
+        self.btnCamera.clicked.connect(self.startCamera)
+        self.btnLogout.clicked.connect(self.logout)
 
         self.window_width = self.ImgWidget.frameSize().width()
         self.window_height = self.ImgWidget.frameSize().height()
@@ -200,12 +273,12 @@ class MainPage(QtWidgets.QMainWindow, form_4):
         self.setWindowTitle('Menubar')
         self.show()
 
-    def log_out(self):
+    def logout(self):
         self.window = Start()
         self.window.show()
         self.close()
 
-    def start_clicked(self):
+    def startCamera(self):
         global running
         running = True
         capture_thread.start()
@@ -214,7 +287,7 @@ class MainPage(QtWidgets.QMainWindow, form_4):
 
     def update_frame(self):
         if not q.empty():
-            self.btnCamera.setText('Camera is running')
+            self.btnCamera.setText('ON')
             frame = q.get()
             img = frame["img"]
 
@@ -237,6 +310,20 @@ class MainPage(QtWidgets.QMainWindow, form_4):
         global running
         running = False
 
+
+# 메시지박스(알림창) 함수
+def messageBox(title, text, style):
+    return ctypes.windll.user32.MessageBoxW(None, text, title, style)
+
+
+# style 인자에 따라 생김새 지정 가능
+# 0: 확인(1)
+# 1: 확인(1), 취소(2)
+# 2: 중단(3), 다시시도(4), 무시(5)
+# 3: 예(6), 아니오(7), 취소(2)
+# 4: 예(6), 아니오(7)
+# 5: 다시시도(4), 취소(2)
+# 6: 취소(2), 다시시도(10), 계속(11)
 
 if __name__ == '__main__':
     capture_thread = threading.Thread(target=grab, args=(0, q, 1920, 1080, 30))
