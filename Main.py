4715410e06bfdf7a5f2b4f48b28ebcc14357cf6a
kimsoyeong <iloveit356@gmail.com>
2020-08-11 02:53:33 +0900

    자세측정 결합

--- Main.py
+++ Main.py
@@ -10,9 +10,11 @@ from ftplib import FTP
 from io import BytesIO
 
 import cv2
-from PyQt5 import QtCore, QtGui, uic, QtWidgets
-from PyQt5.QtWidgets import *
+import matplotlib.pyplot as plt
+import numpy as np
+from PyQt5 import QtCore, uic, QtWidgets, QtGui
 from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 
 # load both ui file
 uifile_1 = 'Start.ui'  # start
@@ -189,31 +191,129 @@ def readServerData(fileName):
 
 # Main Page - Camera
 running = False
-capture_thread = None
+# capture_thread = None
 q = queue.Queue()
 
+MODE = "COCO"
+if MODE is "COCO":
+    protoFile = "pose/coco/pose_deploy_linevec.prototxt"
+    weightsFile = "pose/coco/pose_iter_440000.caffemodel"
+    nPoints = 18
+    POSE_PAIRS = [[1, 0], [1, 2], [1, 5], [2, 3], [3, 4], [5, 6], [6, 7], [1, 8], [8, 9], [9, 10], [1, 11], [11, 12],
+                  [12, 13], [0, 14], [0, 15], [14, 16], [15, 17]]
+# elif MODE is "MPI":
+#     protoFile = "pose/mpi/pose_deploy_linevec_faster_4_stages.prototxt"
+#     weightsFile = "pose/mpi/pose_iter_160000.caffemodel"
+#     nPoints = 15
+#     POSE_PAIRS = [[0, 1], [1, 2], [2, 3], [3, 4], [1, 5], [5, 6], [6, 7], [1, 14], [14, 8], [8, 9], [9, 10], [14, 11],
+#                   [11, 12], [12, 13]]
+# vid_writer = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10,
+# (frame.shape[1], frame.shape[0]))
+net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
 
+
+# shoulderResult = []  # 어깨 측정 list
+
+# 스레드 돌아가는 함수
 def grab(cam, queue, width, height, fps):
+    shoulderResult = []  # 어깨 측정 list
     global running
     capture = cv2.VideoCapture(cam)
     # capture.set(cv2.CAP_PROP_FRAME_WIDTH, width)
     # capture.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
     # capture.set(cv2.CAP_PROP_FPS, fps)
+    inWidth = 368
+    inHeight = 368
+    threshold = 0.1
 
     while (running):
         frame = {}
-        capture.grab()
-        retval, img = capture.retrieve(0)
-        # 카메라 캡쳐 이미지 위에 도형 그리기
-        cv2.circle(img, (40, 60), 15, (255, 0, 0), 10)
+        capture.grab()  # 재귀X: VideoCapture 내장 함수
+        retval, img = capture.retrieve(0)  # grab한 프레임을 decode하여 반환
+
+        imgCopy = np.copy(img)
+        if not retval:
+            cv2.waitKey()
+            break
+
+        imgWidth = img.shape[1]
+        imgHeight = img.shape[0]
+
+        inpBlob = cv2.dnn.blobFromImage(img, 1.0 / 255, (inWidth, inHeight),
+                                        (0, 0, 0), swapRB=False, crop=False)
+        net.setInput(inpBlob)
+        output = net.forward()
+
+        H = output.shape[2]
+        W = output.shape[3]
+
+        # Empty list to store the detected keypoints
+        points = []
+
+        for i in range(nPoints):
+            # confidence map of corresponding body's part.
+            probMap = output[0, i, :, :]
+
+            # Find global maxima of the probMap.
+            minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
+
+            # Scale the point to fit on the original image
+            x = (imgWidth * point[0]) / W
+            y = (imgHeight * point[1]) / H
+
+            if prob > threshold:
+                cv2.circle(imgCopy, (int(x), int(y)), 8, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
+                cv2.putText(imgCopy, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2,
+                            lineType=cv2.LINE_AA)
+
+                # Add the point to the list if the probability is greater than the threshold
+                points.append((int(x), int(y)))
+            else:
+                points.append(None)
+
+        temp_a = 0  # Right shoulder
+        temp_b = 0  # left shoulder
+        temp = 0
+        temp2 = 0
+
+        # Draw Skeleton
+        for pair in POSE_PAIRS:
+            partA = pair[0]
+            partB = pair[1]
+
+            if points[partA] and points[partB]:
+                if (partA == 1 and partB == 2) or (
+                        partA == 1 and partB == 5):  # Neck-Right Shoulder, Neck-left Shoulder일 경우
+                    cv2.line(img, points[partA], points[partB], (255, 0, 0), 3, lineType=cv2.LINE_AA)  # 다른 색으로 표시
+                    cv2.circle(img, points[partA], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
+                    cv2.circle(img, points[partB], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
+                    if partB == 2:  # Right Shoulder인 경우 좌표 저장
+                        temp_a = points[partB]
+                    if partB == 5 and temp_a != 0:  # left Shoulder인 경우 좌표 저장
+                        temp_b = points[partB]
+                        temp = temp_a[0] - temp_b[0]  # 기울기 구하기 위하여 x증가량
+                        temp2 = temp_a[1] - temp_b[1]  # 기울기 구하기 위하여 y증가량
+                        shoulderResult.append(abs(temp2) / abs(temp))  # 절대값을 이용하여 기울기를 구한 후 list에 저장
+
+                else:
+                    cv2.line(img, points[partA], points[partB], (0, 255, 255), 3, lineType=cv2.LINE_AA)
+                    cv2.circle(img, points[partA], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
+                    cv2.circle(img, points[partB], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
+        # vid_writer.write(frame)
 
         frame["img"] = img
-
         if queue.qsize() < 10:
             queue.put(frame)
         else:
-            print
-            queue.qsize()
+            print(queue.qsize())
+
+
+# vid_writer.release()
+
+
+def graph(list):
+    plt.plot(list)  # 그래프 시각화
+    plt.show()  # 그래프 표시
 
 
 class OwnImageWidget(QtWidgets.QWidget):
@@ -238,15 +338,17 @@ class OwnImageWidget(QtWidgets.QWidget):
 class MainPage(QtWidgets.QMainWindow, form_4):
     userid = ''
     username = ''
+    capture_thread = None
 
     def __init__(self, parent=None):
         QtWidgets.QMainWindow.__init__(self, parent)
         self.setupUi(self)
         self.setWindowTitle('MYFIT USER\'S PAGE')
+        MainPage.capture_thread = threading.Thread(target=grab, args=(0, q, 1920, 1080, 30))
 
         # login한 user의 이름 표시
-        MainPage.userid = loginPage.userid  # loginData['id']
-        MainPage.username = loginPage.username  # loginData['name']
+        MainPage.userid = loginPage.userid
+        MainPage.username = loginPage.username
         self.loginName.setText(MainPage.username)
 
         self.btnCamera.clicked.connect(self.startCamera)
@@ -311,16 +413,14 @@ class MainPage(QtWidgets.QMainWindow, form_4):
         return 0
 
     def logout(self):
-        # self.window = Start()
-        # self.window.show()
         self.close()
 
     def startCamera(self):
         global running
         running = True
-        capture_thread.start()
         self.btnCamera.setEnabled(False)
         self.btnCamera.setText('Loading...')
+
     # TODO: Camera OFF
 
     def update_frame(self):
@@ -369,7 +469,6 @@ def removeLogfile(file):
 
 
 if __name__ == '__main__':
-    capture_thread = threading.Thread(target=grab, args=(0, q, 1920, 1080, 30))
     app = QApplication(sys.argv)
     ex = Start()
     ex.show()
